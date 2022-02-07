3287dba859206856d4a86f45be194ab5da008358
rachidelaid <rachidelaid@gmail.com>
2022-02-07 17:28:49 +0100

    add the calculator component

---
+++ src/components/Calculator.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import './Calculator.css';
+
+// eslint-disable-next-line react/prefer-stateless-function
+export default class Calculator extends React.Component {
+  render() {
+    const btns = [
+      'AC',
+      '+/-',
+      '%',
+      '÷',
+      '7',
+      '8',
+      '9',
+      'x',
+      '4',
+      '5',
+      '6',
+      '-',
+      '1',
+      '2',
+      '3',
+      '+',
+      '0',
+      '.',
+      '=',
+    ];
+
+    const operators = '÷x-+=';
+
+    return (
+      <section className="calculator">
+        <div className="input">0</div>
+        <div className="buttons">
+          {btns.map((btn) => (
+            <button
+              type="button"
+              key={btn}
+              id={btn}
+              className={`btn ${operators.includes(btn) ? 'operator' : ''} ${
+                btn === '0' ? 'span-2' : ''
+              }`}
+            >
+              {btn}
+            </button>
+          ))}
+        </div>
+      </section>
+    );
+  }
+}
