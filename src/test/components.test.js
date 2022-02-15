9131e7820d31b911de0b46a31a774d9bc6510362
HAMMAS BIN SALEEM <hammassaleem376@gmail.com>
2022-02-15 19:20:50 +0500

    Added tests to Calculator and Home component

--- src/test/components.test.js
+++ src/test/components.test.js
@@ -4,65 +4,101 @@ import { render, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 import Nav from '../components/Nav';
+import Home from '../components/Home';
+import Calculator from '../components/Calculator';
 
-test('render nav bar', () => {
-  const { container, getByText } = render(
-    <MemoryRouter>
-      <Nav />
-    </MemoryRouter>,
-  );
-  expect(getByText('Math Magicians')).toBeInTheDocument();
-  expect(container.firstChild).toMatchInlineSnapshot(`
-    <nav>
-      <h1>
-        Math Magicians
-      </h1>
-      <ul>
-        <li>
-          <a
-            aria-current="page"
-            class="active"
-            href="/"
-          >
-            Home
-          </a>
-        </li>
-        <li>
-          <a
-            class=""
-            href="/calculator"
-          >
-            Calculator
-          </a>
-        </li>
-        <li>
-          <a
-            class=""
-            href="/quote"
-          >
-            Quotes
-          </a>
-        </li>
-      </ul>
-    </nav>
-  `);
-});
+describe('Navbar Test',()=>{
+  test('render nav bar', () => {
+    const { container, getByText } = render(
+      <MemoryRouter>
+        <Nav />
+      </MemoryRouter>,
+    );
+    expect(getByText('Math Magicians')).toBeInTheDocument();
+    expect(container.firstChild).toMatchInlineSnapshot(`
+      <nav>
+        <h1>
+          Math Magicians
+        </h1>
+        <ul>
+          <li>
+            <a
+              aria-current="page"
+              class="active"
+              href="/"
+            >
+              Home
+            </a>
+          </li>
+          <li>
+            <a
+              class=""
+              href="/calculator"
+            >
+              Calculator
+            </a>
+          </li>
+          <li>
+            <a
+              class=""
+              href="/quote"
+            >
+              Quotes
+            </a>
+          </li>
+        </ul>
+      </nav>
+    `);
+  });
+  
+  test('test link click', () => {
+    render(
+      <MemoryRouter>
+        <Nav />
+      </MemoryRouter>,
+    );
+    fireEvent.click(screen.getByText('Quotes'));
+  
+    expect(screen.getByText('Quotes')).toMatchInlineSnapshot(`
+      <a
+        aria-current="page"
+        class="active"
+        href="/quote"
+      >
+        Quotes
+      </a>
+    `);
+  });
+})
 
-test('test link click', () => {
-  render(
-    <MemoryRouter>
-      <Nav />
-    </MemoryRouter>,
-  );
-  fireEvent.click(screen.getByText('Quotes'));
+describe('Testing Home Component',()=>{
+  test('render homepage',()=>{
+    render(
+        <Home />  
+  )
+  expect(screen.getByText('Welcome to our page!')).toMatchInlineSnapshot(`
+  <h3>
+    Welcome to our page!
+  </h3>
+  `)
+}
+  )}
+)
 
-  expect(screen.getByText('Quotes')).toMatchInlineSnapshot(`
-    <a
-      aria-current="page"
-      class="active"
-      href="/quote"
-    >
-      Quotes
-    </a>
-  `);
-});
+describe('Testing Calculator Component',()=>{
+  test('render homepage',()=>{
+    render(
+        <Calculator />  
+  )
+  fireEvent.click(screen.getByText('5'));
+  fireEvent.click(screen.getByText('4'));
+  expect(screen.getByText('54')).toMatchInlineSnapshot(`
+  <div
+    class="input"
+  >
+    54
+  </div>
+  `)
+}
+  )}
+)
